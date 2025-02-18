0760e5995ba3330a3c5611df8f71f0ceab597457
Nanamib <nanamibuchanan@gmail.com>
2025-02-18 16:06:29 -0600

    left edit

--- animations.js
+++ animations.js
@@ -22,13 +22,13 @@ $(".thin-border").click(function(event){
 
     if(insides.is(":visible")){
         insides.slideUp("slow");
-        $(this).find("h4").animate({left:"0"}, "slow");
+        $(this).find("h1, h4").animate({left:"0"}, "slow");
         $(this).find(".date").animate({left:"0"}, "slow");
         $(this).animate({backgroundColor:"rgb(154, 153, 153)"},"slow");
     }
     else{
         insides.slideDown("slow");
-        $(this).find("h4").animate({left:"35%"}, "slow");
+        $(this).find("h1, h4").animate({left:"35%"}, "slow");
         $(this).find(".date").animate({left:"-40%"}, "slow");
         $(this).animate({backgroundColor:"rgb(198, 198, 198)"},"slow");
     }
