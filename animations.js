9d27baef271b0f2e67dac926b2b420081607f0e7
Nanamib <nanamibuchanan@gmail.com>
2025-02-18 15:39:53 -0600

    fix animations

--- animations.js
+++ animations.js
@@ -1,29 +1,35 @@
+
 $(".thick-border").click(function(){
     var inside = $(this).find("#inside");
 
     if(inside.is(":visible")){
         inside.slideUp("slow");
         $(this).find("h1, h4").animate({left:"0"}, "slow");
-        $(this).find("#date").animate({left:"0"}, "slow");
+        $(this).find(".date").animate({left:"0"}, "slow");
+        $(this).animate({backgroundColor:"rgb(154, 153, 153)"},"slow");
     }
     else{
         inside.slideDown("slow");
-        $(this).find("h1, h4").animate({left:"40%"}, "slow");
-        $(this).find("#date").animate({left:"-40%"}, "slow");
+        $(this).find("h1, h4").animate({left:"35%"}, "slow");
+        $(this).find(".date").animate({left:"-40%"}, "slow");
+        $(this).animate({backgroundColor:"rgb(198, 198, 198)"},"slow");
     }
 });
 
-$(".thin-border").click(function(){
+$(".thin-border").click(function(event){
+    event.stopPropagation();
     var insides = $(this).find("#insides");
 
     if(insides.is(":visible")){
         insides.slideUp("slow");
         $(this).find("h4").animate({left:"0"}, "slow");
-        $(this).find("#date").animate({left:"0"}, "slow");
+        $(this).find(".date").animate({left:"0"}, "slow");
+        $(this).animate({backgroundColor:"rgb(154, 153, 153)"},"slow");
     }
     else{
-        inside.slideDown("slow");
-        $(this).find("h4").animate({left:"40%"}, "slow");
-        $(this).find("#date").animate({left:"-40%"}, "slow");
+        insides.slideDown("slow");
+        $(this).find("h4").animate({left:"35%"}, "slow");
+        $(this).find(".date").animate({left:"-40%"}, "slow");
+        $(this).animate({backgroundColor:"rgb(198, 198, 198)"},"slow");
     }
 });
